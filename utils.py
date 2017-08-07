c3c1b9ea791e3a8a26ae3c0961db43a4adbc8cd7
ricsonc <ricsonc@andrew.cmu.edu>
2017-08-06 23:12:39 -0700

    nancheck

--- utils.py
+++ utils.py
@@ -3,9 +3,13 @@ import numpy as np
 def check_vals(mat, _min = -100.0, _max = 100.0):
     if np.min(mat) < _min:
         print mat
-        assert False, 'value was too small!'
+        assert False, 'value was too small'
 
     if np.max(mat) > _max:
         print mat
-        assert False, 'value was too large!'
-        
+        assert False, 'value was too large'
+
+    if np.isnan(mat).any():
+        print mat
+        assert False, 'value has nans'
+    
